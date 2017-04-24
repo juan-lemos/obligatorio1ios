8adbf3944ed8ab288c7b7c1a0f11112775b1fbba
JuanPablo <juanpablo@JuanPablos-MacBook-Pro.local>
2017-04-23 22:06:37 -0300

    add image from gallery

---
+++ obligatorio/ItemViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+
+class ItemViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var chooseButton: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+    
+    @IBAction func btnClicked(_ sender: Any) {
+    
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            print("Button capture")
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView.contentMode = .scaleAspectFit
+        dismiss(animated: true, completion: nil)
+    }
+    
+//    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+//        self.dismiss(animated: true, completion: { () -> Void in
+//            
+//        })
+//        
+//        imageView.image = image
+//    }
+}
